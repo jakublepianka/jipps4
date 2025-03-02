edb702642eb9542e3663a0d75e99412ea7b96d05
Jakub <jakub12lepianka@wp.pl>
2025-03-02 12:39:41 +0100

    added solved lab1 exercises

--- JiPP-S4/Program.cs
+++ JiPP-S4/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        //Rozgrzewka
+        //Zadanie 2.
+        //var name = Console.ReadLine();
+        //Console.WriteLine("Hello " + name);
+
+        //Zadanie 3.
+        //int result = 5 + 9;
+
+        //Operatory
+        //Zadanie 1.   
+        //int number = 1;
+        //float money = 1.04362642346234f;
+        //string text = "tekst";
+        //bool isLogged = true;
+        //char myChar = 'a';
+        //decimal price = 1.0m;
+
+        //Console.WriteLine($"{number}, {money}, {text}, {isLogged}, {myChar}, {price}");
+
+        //Zadanie 2.
+        //string myAge = "Age: ";
+        //int wifeAge = 18;
+        //var result = myAge + wifeAge;
+        //Console.WriteLine(result);
+        //Wniosek: można dodawać dane string z innymi typami danych
+
+        //Zadanie 3.
+        //bool isTrue = true;
+        //bool isFalse = false;
+        //bool isReallyTrue = true;
+
+        //bool and = isTrue && isFalse;
+        //bool or = isTrue || isReallyTrue;
+        //bool negative = !isFalse;
+
+        //Console.WriteLine($"AND: {and}, OR: {or}, NEGATIVE: {negative}");
+
+        //Zadanie 4.
+        //int a = 5;
+        //int b = 12;
+
+        //int add = a + b;
+        //int sub = a - b;
+        //int div = a / b;
+        //int mul = a * b;
+        //int mod = a % b;
+
+        //Console.WriteLine($"ADD: {add}, SUB: {sub}, DIV: {div}, MUL: {mul}, MOD: {mod}");
+
+        //Zadanie 5.
+        //string a = "Ala";
+        //string b = "ma";
+        //string c = "kota";
+        //string result = a + b + c;
+
+        //Console.WriteLine(result);
+        //Wniosek: można dodawać dane typu string do siebie
+
+
+    }
+}
