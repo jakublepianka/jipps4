9a32c58a8f1159af1218011c287369f5660fddbc
Jakub <jakub12lepianka@wp.pl>
2025-03-02 14:51:59 +0100

    added solved lab2 exercises and changed directory structure

---
+++ JiPP-S4/Lab2/Program.cs
@@ -0,0 +1,119 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Zadanie 1.
+        //int n1 = 10;
+        //int n2 = 20;
+        //if (n1 > n2)
+        //{
+        //    Console.WriteLine("n1 jest wieksza niz n2");
+        //}
+        //else if (n2 > n1)
+        //{
+        //    Console.WriteLine("n2 est wieksza niz n1");
+        //}
+        //else
+        //{ 
+        //    Console.WriteLine("n1 jest rowne n2");
+        //}
+
+
+        //Zadanie 2.
+        //int i = 0;
+        //int counter = 0;
+        //for (; i < 10; i++)
+        //{
+        //    counter++;
+        //    Console.WriteLine("C#");
+
+        //}
+
+
+        //while (i > 0)
+        //{
+        //    Console.WriteLine("C#");
+        //    i--;
+        //}
+
+        //Zadanie 3.
+        //int n = 10;
+        //for (int i = 0; i <= n; i++)
+        //{
+
+        //    if (i % 2 == 0)
+        //    {
+        //        Console.WriteLine($"{i} - Parzysta");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"{i} - Nieparzysta");
+        //    }
+        //}
+
+        //Zadanie 4.
+        //int n = 5;
+        //string x = "* ";
+        //for (int i = 0; i < n; i++)
+        //{
+        //    Console.WriteLine(x);
+        //    x = x + "* ";
+        //}
+
+        //Zadanie 5.
+        //string[] exam = new string[100];
+        //do
+        //{
+        //    int counter = 0;
+        //    string mark = "";
+        //    int point;
+        //    int.TryParse(Console.ReadLine(), out point);
+        //    if (point < 0 || point > 100)
+        //    {
+        //        Console.WriteLine("Wartosc poza zakresem");
+        //    }
+        //    else if (point < 40)
+        //    {
+        //        mark = "Ocena niedostateczna";
+        //        Console.WriteLine(mark);
+        //        exam[counter] += mark;
+        //    }
+        //    else if (point < 54)
+        //    {
+        //        mark = "Ocena dopuszczajaca";
+        //        Console.WriteLine(mark);
+        //        exam[counter] += mark;
+        //    }
+        //    else if (point < 69)
+        //    {
+        //        mark = "Ocena dostateczna";
+        //        Console.WriteLine(mark);
+        //        exam[counter] += mark;
+        //    }
+        //    else if (point < 84)
+        //    {
+        //        mark = "Ocena dobra";
+        //        Console.WriteLine(mark);
+        //        exam[counter] += mark;
+        //    }
+        //    else if (point < 99)
+        //    {
+        //        mark = "Ocena bardzo dobra";
+        //        Console.WriteLine(mark);
+        //        exam[counter] += mark;
+        //    }
+        //    else
+        //    {
+        //        mark = "Ocena celujaca";
+        //        Console.WriteLine(mark);
+        //        exam[counter] += mark;
+        //    }
+
+        //        counter++;
+
+        //} while (true);
+
+    }
+
+
+}
