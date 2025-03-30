91a262e14ea0bb2a901d521bd31d8f304e37f2bf
Jakub <jakub12lepianka@wp.pl>
2025-03-30 12:51:50 +0200

    Completed Lab3

--- JiPP-S4/Lab3/Lab3/Program.cs
+++ JiPP-S4/Lab3/Lab3/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] colors = { "Czerwony", "Zielony", "Niebieski", "Żolty"};
+        
+        Console.WriteLine("Mój pierwszy kolor to: " + colors[0]);
+        Console.WriteLine("Mój ostatni kolor to: " + colors[3]);
+
+        int[] liczby = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        for (int i = 0; i < liczby.Length ; i++)
+        {
+            Console.WriteLine("Liczba: " + liczby[i]);
+        }
+        foreach (int liczba in liczby)
+        {
+            Console.WriteLine("Liczba: " + liczba);
+        }
+        int counter = 0;
+        while (counter < 10)
+        {
+
+            Console.WriteLine("Liczba: " + liczby[counter]);
+            counter++;
+        }
+
+        List<string> fruits = new List<string>();
+        fruits.Add("Jablko");
+        fruits.Add("Gruszka");
+        fruits.Add("Banan");
+        fruits.Add("Pomarancza");
+        
+        Console.WriteLine(string.Join(", ", fruits));
+        fruits.Remove("Jablko");
+        fruits.RemoveAt(fruits.Count - 1);
+        Console.WriteLine(string.Join(", ", fruits));
+
+    }
+
+
+}
